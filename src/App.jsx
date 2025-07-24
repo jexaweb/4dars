e28686d0195239c172102d480e9560102d06fcc2
kamoliddin <kamoliddin@gmail.com>
2025-07-25 00:12:11 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,17 @@
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Cart from "./components/Cart";
 
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
 
 function App() {
-  const [data, setDeta] = useState(null);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((data) => data.json())
-      .then((data) => setDeta(data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  const delite = (id) => {
-    setDeta((prev) => {
-      return {
-        ...prev,
-        products: prev.products.filter((product) => product.id !== id),
-      };
-    });
-  };
-
   return (
     <div>
       <Header />
-
-      <ul className="flex flex-wrap gap-4 justify-center p-4 mt-30 mb-8">
-        {data?.products?.map((product) => (
-          <li key={product.id} className="w-80 bg-amber-50 shadow rounded">
-            <figure className="px-6 pt-6">
-              <img
-                src={product.images[0]}
-                alt={product.title}
-                className="rounded-xl w-full h-48"
-              />
-            </figure>
-            <div className="card-body items-center text-center text-black">
-              <h2 className="card-title">{product.title}</h2>
-              <p className="line-clamp-4">{product.description}</p>
-              <p>Category: {product.category}</p>
-              <p>Brand: {product.brand}</p>
-              <p>Rating: {product.rating}</p>
-              <p>Price: ${product.price}</p>
-              <div className="card-actions">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => delite(product.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-
+      <Cart />
       <Footer />
     </div>
   );
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,41 +1,54 @@
-// import { useEffect, useState } from "react";
-// function Cart() {
-//   const [cart, setCard] = useState();
-//   const cartEl = setCard(!cart);
+import { useEffect, useState } from "react";
+function Cart() {
+  const [data, setDeta] = useState(null);
 
-//   return (
-//     <div key={product.id}>
-//       <ul className="flex flex-wrap gap-4 justify-center p-4 mt-30 mb-8">
-//         {data.products.map((product) => (
-//           <li key={product.id} className="w-80 bg-amber-50 shadow rounded">
-//             <figure className="px-6 pt-6">
-//               <img
-//                 src={product.images[0]}
-//                 alt={product.title}
-//                 className="rounded-xl w-full h-48 "
-//               />
-//             </figure>
-//             <div className="card-body items-center text-center text-black">
-//               <h2 className="card-title">{product.title}</h2>
-//               <p className="line-clamp-4">{product.description}</p>
-//               <p>Category: {product.category}</p>
-//               <p>Brand: {product.brand}</p>
-//               <p>Rating: {product.rating}</p>
-//               <p>Price: ${product.price}</p>
-//               <div className="card-actions">
-//                 <button
-//                   className="btn btn-primary"
-//                   onClick={() => delite(product.id)}
-//                 >
-//                   Delete
-//                 </button>
-//               </div>
-//             </div>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((data) => data.json())
+      .then((data) => setDeta(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-// export default Cart;
+  const delite = (id) => {
+    setDeta((prev) => {
+      return {
+        ...prev,
+        products: prev.products.filter((product) => product.id !== id),
+      };
+    });
+  };
+
+  return (
+    <ul className="flex flex-wrap gap-4 justify-center p-4 mt-30 mb-8">
+      {data?.products?.map((product) => (
+        <li key={product.id} className="w-80 bg-amber-50 shadow rounded">
+          <figure className="px-6 pt-6">
+            <img
+              src={product.images[0]}
+              alt={product.title}
+              className="rounded-xl w-full h-48"
+            />
+          </figure>
+          <div className="card-body items-center text-center text-black">
+            <h2 className="card-title">{product.title}</h2>
+            <p className="line-clamp-4">{product.description}</p>
+            <p>Category: {product.category}</p>
+            <p>Brand: {product.brand}</p>
+            <p>Rating: {product.rating}</p>
+            <p>Price: ${product.price}</p>
+            <div className="card-actions">
+              <button
+                className="btn btn-primary"
+                onClick={() => delite(product.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Cart;
