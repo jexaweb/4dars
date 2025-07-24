ce71fd990abe7e1607d2b865a21257654b5c34a7
kamoliddin <kamoliddin@gmail.com>
2025-07-25 00:03:22 +0500

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,19 @@ function Header() {
   return (
     <header className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-        <div className="text-2xl font-bold text-blue-600"></div>
-        <img className="w30  h11" src="./img/logo_coupang_w350.png" alt="" />
+        <div className="text-2xl font-bold text-blue-600">
+          {/* <img className="w30  h11" src="./img/logo_coupang_w350.png" alt="" /> */}
+          <div className="text-7xl font-bold mb-1">
+            <span className="text-[#5C1D1D]">c</span>
+            <span className="text-[#5C1D1D]">o</span>
+            <span className="text-[#E53935]">u</span>
+            <span className="text-[#FBC02D]">p</span>
+            <span className="text-[#8BC34A]">a</span>
+            <span className="text-[#4FC3F7]">n</span>
+            <span className="text-[#4FC3F7]">g</span>
+          </div>
+        </div>
+
         <nav className="hidden md:flex gap-6 text-gray-700 font-medium">
           <a href="#" className="hover:text-blue-600">
             Home
