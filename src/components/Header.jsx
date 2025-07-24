1d542dbe784d44fc1ac46a33afddb5c27ecd2d5f
kamoliddin <kamoliddin@gmail.com>
2025-07-24 23:54:18 +0500

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
     <header className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
         <div className="text-2xl font-bold text-blue-600">
-          <img className="w30  h10" src="./img/logo_coupang_w350.png" alt="" />
+          <img className="w30  h11" src="./img/logo_coupang_w350.png" alt="" />
         </div>
 
         <nav className="hidden md:flex gap-6 text-gray-700 font-medium">
